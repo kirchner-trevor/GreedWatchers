6580bd170f880222c884af9624ce68df07a16452
Trevor <kirchner.trevor@gmail.com>
2024-01-25 21:50:14 -0600

    Added material ui.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+import {
+  Container,
+  Box,
+  Typography,
+  AppBar,
+  IconButton,
+  Toolbar,
+} from "@mui/material";
+import { Menu } from "@mui/icons-material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <Menu />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+            Photos
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="sm">
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+            Material UI Create React App example in TypeScript
+          </Typography>
+        </Box>
+      </Container>
+    </>
   );
 }
 
